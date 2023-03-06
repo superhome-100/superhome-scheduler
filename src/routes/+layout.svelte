d8eb33fbe08550bce1038658666b7624d5174219
neil <neil@neils-MacBook-Pro.local>
2023-03-06 20:34:06 +0800

    organize files into proper directory

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
-<script>
-    import './styles.css';
+<script lang="js">
+    import '../styles.css';
 </script>
 
-<slot />
+<div id="app">
+    <div id="category_buttons">
+        <a href="/">
+            <button>My Reservations</button>
+        </a>
+        <a href="/pool">
+            <button>Pool</button>
+        </a>
+        <a href="/open-water">
+            <button>Open Water</button>
+        </a>
+        <a href="/classroom">
+            <button>Classroom</button>
+        </a>
+    </div>
+    <slot />
+</div>
