a8707eb85eaaed5ced0ec98b4fe9f51fed4d3a79
Michael Horgan <michaelghorgan@gmail.com>
2023-03-16 08:19:17 +0800

    fix bug in multi-day reservation display -> show all rsvs on that day, not just first

--- src/lib/components/DayOfMonth.svelte
+++ src/lib/components/DayOfMonth.svelte
@@ -2,7 +2,7 @@
     import { view, viewedDate } from '$lib/stores.js';
     
     export let date;
-    export let rsv;
+    export let rsvs;
     let id_internal = null;
     export {id_internal as id};
     export let category;
@@ -16,9 +16,12 @@
 
 <a href="/single-day/{category}" style="text-decoration: none">
     <div style="height:100%" on:click={handleClick} on:keypress={handleClick}>
-        <p style="margin: auto; width: 25px; text-align: center" id={id_internal}>{date.getDate()}</p>
-        {#if rsv}
+        <p 
+            style="margin: auto; width: 25px; text-align: center" 
+            id={id_internal}>{date.getDate()}
+        </p>
+        {#each rsvs as rsv}
             <p class="rsv">{rsv.user.name}</p>
-        {/if}
+        {/each}
     </div>
 </a>
