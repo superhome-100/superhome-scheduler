440122c0b7c057503e223d11bacced5ee8a157e3
Michael Horgan <michaelghorgan@gmail.com>
2023-03-19 06:19:16 +0800

    fix bug in MyReservations date-sorting function

--- src/lib/components/ReservationDialog.svelte
+++ src/lib/components/ReservationDialog.svelte
@@ -2,10 +2,15 @@
     import { getContext } from 'svelte';
 	import ReservationForm from './ReservationForm.svelte';
     import { reservations } from '$lib/stores.js';
+    import { minValidDateStr } from '$lib/ReservationTimes.js';
 
-    export let category='openwater';
-    export let dateFn;
-
+    export let category = 'openwater';
+    /* require date to be a fn to enable lazy evaluation since it 
+    might depend on values from the database, which may not have
+    loaded by the time the page renders, e.g. immediately after 
+    a refresh */
+    export let dateFn = minValidDateStr; 
+    
     const { open } = getContext('simple-modal');
 
     export const showDialog = () => {
