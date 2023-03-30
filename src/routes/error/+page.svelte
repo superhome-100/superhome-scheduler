907a1803b067476bae89d9da9801d6b83da774c9
Michael Horgan <michaelghorgan@gmail.com>
2023-03-30 18:22:38 +0800

    add go home button to error page

--- src/routes/error/+page.svelte
+++ src/routes/error/+page.svelte
@@ -1,1 +1,6 @@
-<p style='text-align: center'>Sorry!  We're having trouble communicating with the schedule server right now.  Please try again later.</p>
+<p style='text-align: center'>Sorry!  We're having trouble communicating with the server right now.  Please try again later.</p>
+<div style="width: 100%; text-align: center">
+    <a href='/'>
+        <button>Go Home</button>
+    </a>
+</div>
