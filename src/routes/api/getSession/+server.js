0f4958a7b9684742e13d30be53b0ad8e7cd51227
Michael Horgan <michaelghorgan@gmail.com>
2023-04-03 16:56:25 +0800

    add more error handling for database/server comms issues

--- src/routes/api/getSession/+server.js
+++ src/routes/api/getSession/+server.js
@@ -2,16 +2,20 @@ import { getSession } from '$lib/server/server.js';
 import { json } from '@sveltejs/kit';
 
 export async function GET({ cookies })  {
-    let user;
-    let session = cookies.get('sessionid');
-    if (session !== undefined) {
-        let record = await getSession(session);
-        if (record == undefined) {
-            // cookie is invalid -> delete it
-            cookies.delete('sessionid', { path: '/' });
-        } else {
-            user = record.user;
+    try {
+        let user;
+        let session = cookies.get('sessionid');
+        if (session !== undefined) {
+            let record = await getSession(session);
+            if (record == undefined) {
+                // cookie is invalid -> delete it
+                cookies.delete('sessionid', { path: '/' });
+            } else {
+                user = record.user;
+            }
         }
+        return json({ status: 'success', user });
+    } catch (error) {
+        return json({ status: 'error', error });
     }
-    return json({user});
 }
--- src/routes/api/login/+server.js
+++ src/routes/api/login/+server.js
@@ -2,13 +2,17 @@ import { json } from '@sveltejs/kit';
 import { authenticateUser, createSession } from '$lib/server/server.js';
 
 export async function POST({ cookies, request })  {
-    const { userId, userName } = await request.json();
-    const record = await authenticateUser(userId, userName);
-    if (record.status === 'active') {
-        if (cookies.get('sessionid') === undefined) {
-            const session = await createSession(record);
-            cookies.set('sessionid', session.id, {path: '/'});
+    try {
+        const { userId, userName } = await request.json();
+        const record = await authenticateUser(userId, userName);
+        if (record.status === 'active') {
+            if (cookies.get('sessionid') === undefined) {
+                const session = await createSession(record);
+                cookies.set('sessionid', session.id, {path: '/'});
+            }
         }
+        return json({ status: 'success', record });
+    } catch (error) {
+        return json({ status: 'error', error });
     }
-    return json(record);
 }
