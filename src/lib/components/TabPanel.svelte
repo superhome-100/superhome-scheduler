cb466506ef51f9cf31691cde4118f99def414133
Michael Horgan <michaelghorgan@gmail.com>
2023-04-20 06:53:57 +0800

    use tabs for viewing multiple reservations at once

--- src/lib/components/TabPanel.svelte
+++ src/lib/components/TabPanel.svelte
@@ -1,8 +1,8 @@
 <script>
 	import { getContext } from 'svelte';
 	import { TABS } from './Tabs.svelte';
-
-	const panel = {};
+    
+    const panel = {};
 	const { registerPanel, selectedPanel } = getContext(TABS);
 
 	registerPanel(panel);
