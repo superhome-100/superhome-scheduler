4095b745e65046ed38c355db9d1993b7b027c4c8
Michael Horgan <michaelghorgan@gmail.com>
2023-05-01 18:29:19 +0800

    try calling createOrUpdate per entry instead of doing bulk submission

--- src/routes/api/cron/+server.js
+++ src/routes/api/cron/+server.js
@@ -1,19 +1,24 @@
 import { XataClient } from '$lib/server/xata.codegen.server.js';
 import { XATA_API_KEY } from '$env/static/private';
 
-export async function GET(req) {
+export async function GET() {
     const main = new XataClient({ apiKey: XATA_API_KEY });
     const backup = new XataClient({ apiKey: XATA_API_KEY, branch: 'backup' });
-    console.log(backup);
     let reservations = await main.db.Reservations.getAll();
-    console.log("backing up " + reservations.length + " reservations");
-    console.log(reservations[0]);
-    await backup.db.Reservations.createOrUpdate(reservations);
+    for (let i=0; i<reservations.length; i++) {
+        if (i % 50 == 0) {
+            setTimeout(()=>{}, 1000);
+        }
+        let {id, ...rest} = reservations[i];
+        await backup.db.Reservations.createOrUpdate(id, rest);
+    }
 
     let users = await main.db.Users.getAll();
-    console.log('backing up ' + users.length + ' users');
-    await backup.db.Users.createOrUpdate(users);
-
-    let now = new Date();
-    return new Response('Backed up DB at ' + now);
+    for (let i=0; i < users.length; i++) {
+        if (i % 50 == 0) {
+            setTimeout(()=>{}, 1000);
+        }
+        let {id, ...rest} = users[i];
+        await backup.db.Users.createOrUpdate(id, rest);
+    }
 }
