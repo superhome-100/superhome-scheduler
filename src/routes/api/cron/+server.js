f9f57bda293e07d0e86a3b493c2bf9c6333cfbca
Michael Horgan <michaelghorgan@gmail.com>
2023-05-01 18:17:53 +0800

    more debug prints

--- src/routes/api/cron/+server.js
+++ src/routes/api/cron/+server.js
@@ -4,9 +4,10 @@ import { XATA_API_KEY } from '$env/static/private';
 export async function GET(req) {
     const main = new XataClient({ apiKey: XATA_API_KEY });
     const backup = new XataClient({ apiKey: XATA_API_KEY, branch: 'backup' });
-
+    console.log(backup);
     let reservations = await main.db.Reservations.getAll();
     console.log("backing up " + reservations.length + " reservations");
+    console.log(reservations[0]);
     await backup.db.Reservations.createOrUpdate(reservations);
 
     let users = await main.db.Users.getAll();
