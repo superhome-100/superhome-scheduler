6086761833a318127d29579b1367b63235ec80b6
Michael Horgan <michaelghorgan@gmail.com>
2023-05-01 18:32:05 +0800

    backup Users first then Reservations

--- src/routes/api/cron/+server.js
+++ src/routes/api/cron/+server.js
@@ -4,14 +4,6 @@ import { XATA_API_KEY } from '$env/static/private';
 export async function GET() {
     const main = new XataClient({ apiKey: XATA_API_KEY });
     const backup = new XataClient({ apiKey: XATA_API_KEY, branch: 'backup' });
-    let reservations = await main.db.Reservations.getAll();
-    for (let i=0; i<reservations.length; i++) {
-        if (i % 50 == 0) {
-            setTimeout(()=>{}, 1000);
-        }
-        let {id, ...rest} = reservations[i];
-        await backup.db.Reservations.createOrUpdate(id, rest);
-    }
 
     let users = await main.db.Users.getAll();
     for (let i=0; i < users.length; i++) {
@@ -20,5 +12,14 @@ export async function GET() {
         }
         let {id, ...rest} = users[i];
         await backup.db.Users.createOrUpdate(id, rest);
+    }
+
+    let reservations = await main.db.Reservations.getAll();
+    for (let i=0; i<reservations.length; i++) {
+        if (i % 50 == 0) {
+            setTimeout(()=>{}, 1000);
+        }
+        let {id, ...rest} = reservations[i];
+        await backup.db.Reservations.createOrUpdate(id, rest);
     }
 }
