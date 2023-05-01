ad3358d2fc873d8df65af8457b6d04cfb6b8f633
Michael Horgan <michaelghorgan@gmail.com>
2023-05-01 17:42:01 +0800

    add cron job for backing up database

---
+++ src/routes/api/cron/+server.js
@@ -0,0 +1,16 @@
+import { XataClient } from '$lib/server/xata.codegen.server.js';
+import { XATA_API_KEY } form '$env/static/private';
+
+export async function GET(req) {
+    const main = new XataClient({ apiKey: XATA_API_KEY });
+    const backup = new XataClient({ apiKey: XATA_API_KEY, branch: 'backup' });
+
+    let reservations = await main.db.Reservations.getAll();
+    await backup.db.Reservations.createOrUpdate(reservations);
+
+    let users = await main.db.Users.getAll();
+    await backup.db.Users.createOrUpdate(users);
+
+    let now = new Date();
+    return new Response('Backed up DB at ' + now);
+}
