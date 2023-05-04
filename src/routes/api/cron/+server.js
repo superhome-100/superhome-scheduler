8357c5f5b943ba9833215859dfde3912127e4a13
Michael Horgan <michaelghorgan@gmail.com>
2023-05-04 10:24:43 +0800

    use backup-day-1 and backup-day-2 for cron job

--- src/routes/api/cron/+server.js
+++ src/routes/api/cron/+server.js
@@ -2,16 +2,19 @@ import { XataClient } from '$lib/server/xata.codegen.server.js';
 import { XATA_API_KEY } from '$env/static/private';
 
 export async function GET() {
-    const main = new XataClient({ apiKey: XATA_API_KEY });
-    const backup = new XataClient({ apiKey: XATA_API_KEY, branch: 'backup' });
+    const main = new XataClient({ apiKey: XATA_API_KEY, branch: 'main' });
+    const backup1 = new XataClient({ apiKey: XATA_API_KEY, branch: 'backup-day-1' });
+    const backup2 = new XataClient({ apiKey: XATA_API_KEY, branch: 'backup-day-2' });
 
-    let users = await main.db.Users.getAll();
-    await backup.db.Users.createOrUpdate(users);
+    for (let [from, to] of [[backup1, backup2]]) {
+        let users = await from.db.Users.getAll();
+        await to.db.Users.createOrUpdate(users);
 
-    let reservations = await main.db.Reservations.getAll();
-    for (let i=0; i<reservations.length; i++) {
-        let {user, ...rest} = reservations[i];
-        reservations[i] = {...rest, user: user.id};
+        let reservations = await from.db.Reservations.getAll();
+        for (let i=0; i<reservations.length; i++) {
+            let {user, ...rest} = reservations[i];
+            reservations[i] = {...rest, user: user.id};
+        }
+        await to.db.Reservations.createOrUpdate(reservations);
     }
-    await backup.db.Reservations.createOrUpdate(reservations);
 }
