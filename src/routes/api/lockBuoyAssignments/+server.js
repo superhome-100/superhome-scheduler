ade854b062ad0aa1b4670a26564e9e6d0e349e17
Michael Horgan <michaelghorgan@gmail.com>
2023-05-05 13:24:40 +0800

    lock buoy from client: initial implementation

--- src/routes/api/lockBuoyAssignments/+server.js
+++ src/routes/api/lockBuoyAssignments/+server.js
@@ -1,30 +1,37 @@
 import { getXataClient } from '$lib/server/xata.js';
 import { datetimeToLocalDateStr } from '$lib/datetimeUtils.js';
-import { buoys } from '$lib/stores.js';
-import { get } from 'svelte/store';
 import { assignRsvsToBuoys } from '$lib/autoAssignOpenWater.js';
+import { json } from '@sveltejs/kit';
 
 const xata = getXataClient();
 
-export async function GET() {
-    let tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate()+1);
-    let tomStr = datetimeToLocalDateStr(tomorrow);
-    let rsvs = await xata.db.Reservations
-        .filter({ date: tomStr, category: 'openwater', status: { $any: ['pending', 'confirmed'] }})
-        .getAll();
+export async function POST() {
+    try {
+        let tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate()+1);
+        let tomStr = datetimeToLocalDateStr(tomorrow);
+        let rsvs = await xata.db.Reservations
+            .filter({
+                date: tomStr,
+                category: 'openwater',
+                status: { $any: ['pending', 'confirmed'] }
+            }).getAll();
+        let buoys = await xata.db.Buoys.getAll();
+        let updates = []
+        for (let owTime of ['AM', 'PM']) {
+            let result = assignRsvsToBuoys(buoys, rsvs.filter(rsv => rsv.owTime === owTime));
+            for (let buoy of buoys) {
+                let toAsn = result.assignments[buoy.name]
+                if (toAsn != undefined) {
+                    updates.push(...toAsn.map(rsv => { return { id: rsv.id, buoy: buoy.name }}));
 
-    let updates = []
-    for (let owTime of ['AM', 'PM']) {
-        let result = assignRsvsToBuoys(get(buoys), rsvs.filter(rsv => rsv.owTime === owTime));
-        for (let buoy of get(buoys)) {
-            let toAsn = result.assignments[buoy.name]
-            if (toAsn != undefined) {
-                updates.push(...toAsn.map(rsv => { return { id: rsv.id, buoy: buoy.name }}));
-
+                }
             }
-        }
 
+        }
+        let reservations = await xata.db.Reservations.update(updates);
+        return json({ status: 'success', reservations });
+    } catch (error) {
+        return json({ status: 'error', error });
     }
-    await xata.db.Reservations.update(updates);
 }
