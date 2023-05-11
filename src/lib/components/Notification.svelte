b9dc4e11213727523eca1ff2a1302bac05f2a4e3
Michael Horgan <michaelghorgan@gmail.com>
2023-05-11 18:18:04 +0800

    remove start/end dates and detect and honor line breaks in notification messsages

--- src/lib/components/Notification.svelte
+++ src/lib/components/Notification.svelte
@@ -3,7 +3,6 @@
     import { notifications } from '$lib/stores.js';
     import NotificationPopup from '$lib/components/NotificationPopup.svelte';
     import { datetimeToLocalDateStr } from '$lib/datetimeUtils.js';
-    import { onMount } from 'svelte';
 
     const { open } = getContext('simple-modal');
 
@@ -21,15 +20,8 @@
     }
 
     function checkForNotifications(ntfs) {
-        console.log(ntfs.length);
-        let today = datetimeToLocalDateStr(new Date());
         if (ntfs.length > 0) {
-            let ntf = ntfs.pop();
-            if ((ntf.startDate === 'default' || ntf.startDate <= today) 
-                && (ntf.endDate === 'default' || ntf.endDate >= today)
-            ) {
-                showNotification(ntf);
-            }
+            showNotification(ntfs.pop());
         }
     }
 
