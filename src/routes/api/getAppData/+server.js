892de8851551f58ef7c355d4223306a6832bccd4
neil molina <get.neilmolina@gmail.com>
2023-05-27 11:48:28 +0800

    logging

--- src/routes/api/getAppData/+server.js
+++ src/routes/api/getAppData/+server.js
@@ -2,13 +2,17 @@ import { json } from '@sveltejs/kit';
 import { getReservationsSince, getUserActiveNotifications } from '$lib/server/server.js';
 
 import { getAllUsers } from '../../../lib/server/user';
+import { notifications, reservations } from '../../../lib/stores';
 
 export async function POST({ request }) {
 	try {
 		let { user, minDateStr } = await request.json();
-		let notifications = await getUserActiveNotifications(user);
-		const reservations = await getReservationsSince(minDateStr);
-		const users = await getAllUsers();
+		const [notifications, reservations, users] = await Promise.all([
+			getUserActiveNotifications(user),
+			getReservationsSince(minDateStr),
+			getAllUsers()
+		]);
+
 		const usersById = users.reduce((obj, user) => {
 			obj[user.id] = user;
 			return obj;
