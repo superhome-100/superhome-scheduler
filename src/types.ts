1faa0701fd4d1431f8ff094e6d1266672638e441
neil molina <get.neilmolina@gmail.com>
2023-06-02 21:16:56 +0800

    format

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,11 @@
-import type { Reservations } from "./lib/server/xata";
+import type { Reservations } from './lib/server/xata';
 
-export type ReservationData = Omit<Reservations, "category"> & {
-  category: ReservationCategory;
+export type ReservationData = Omit<Reservations, 'category'> & {
+	category: ReservationCategory;
 };
 
 export enum ReservationCategory {
-  openwater,
-  pool,
-  classroom
+	openwater,
+	pool,
+	classroom
 }
