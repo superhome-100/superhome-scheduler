a785668761f990778fcff910451d9e2bff472ebd
neil molina <get.neilmolina@gmail.com>
2023-06-03 14:30:49 +0800

    fix types

--- src/lib/server/xata-old.ts
+++ src/lib/server/xata-old.ts
@@ -1,7 +1,7 @@
 import { XATA_API_KEY, XATA_BRANCH } from '$env/static/private';
 import { XataClient } from './xata';
 
-const instances: {[branch:string]: XataClient} = {};
+const instances: { [branch: string]: XataClient } = {};
 
 export const getXataClient = (branch: string = XATA_BRANCH || 'main') => {
 	if (instances[branch]) return instances[branch];
