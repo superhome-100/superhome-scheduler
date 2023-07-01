7113e6c31d16f83cd471a4c468c66a063a05f9ed
neil molina <get.neilmolina@gmail.com>
2023-07-01 15:10:03 +0800

    initial auth conversion

---
+++ src/lib/api.ts
@@ -0,0 +1,81 @@
+import type { UsersRecord } from './server/xata.codegen';
+
+// TODO: fix this type
+export const getSettings = async () => {
+  const response = await fetch('/api/getSettings');
+  const data = await response.json() as {
+    status: 'success' | 'error';
+    settings: {
+      [key: string]: {
+        default: string;
+        entries: any[];
+      };
+    };
+    buoys: any[];
+  }
+  return data;
+}
+
+// TODO: fix types
+export const getSession = async () => {
+  const response = await fetch('/api/getSession');
+
+  const data = await response.json() as {
+    status: 'success' | 'error';
+    user?: UsersRecord;
+    viewMode?: string;
+    error?: string;
+  }
+
+ return data;
+}
+
+export const getBoatAssignments = async () => {
+  const response = await fetch('/api/getBoatAssignments');
+
+  const data = await response.json() as {
+    status: 'success' | 'error';
+    assignments?: {
+      [key: string]: any
+    };
+    error?: string;
+  }
+
+ return data;
+
+}
+
+export const getUserPastReservations = async (uid: string, maxValidDateStr: string) => {
+  const response = await fetch('/api/getUserPastReservations', {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({ user: uid, maxValidDateStr })
+  });
+  let data = await response.json() as {
+    status: 'success' | 'error';
+    userPastReservations?: any[];
+    error?: string;
+  };
+  return data;
+}
+
+export const getAppData = async (uid: string, minDateStr: string) => {
+  const response = await fetch('/api/getAppData', {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({ user: uid, minDateStr })
+  });
+
+  const data = await response.json() as {
+    status: 'success' | 'error';
+    notifications?: any[];
+    reservations?: any[];
+    usersById?: {
+      [uid: string]: UsersRecord;
+    };
+    error?: string;
+  };
+  return data;
+}
+
+export const 
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,17 +1,18 @@
 import { writable } from 'svelte/store';
+import type { UsersRecord } from './server/xata.codegen';
 
 // TODO: Add specific types for each store
 export const buoys = writable<any[]>([]);
 export const boatAssignments = writable<any>({});
 export const canSubmit = writable<boolean>(false);
-export const loginState = writable<string>('pending');
+export const loginState = writable<'pending' | 'in' | 'out'>('out');
 export const notifications = writable<any[]>([]);
 export const profileSrc = writable<any>(null);
 export const reservations = writable<any[]>([]);
 export const settings = writable<any>({});
-export const user = writable<any>(null);
+export const user = writable<UsersRecord|null>(null);
 export const userPastReservations = writable<any[]>([]);
-export const users = writable<any[]>([]);
+export const users = writable<{ [uid:string]: any }>([]);
 export const view = writable<string>('multi-day');
 export const viewedDate = writable<Date>(new Date());
 export const viewedMonth = writable<Date>(new Date());
