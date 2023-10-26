ad7bbcc1a5029d870b86e93b3aa156a1e00cd35c
Michael Horgan <michael.g.horgan@gmail.com>
2023-10-26 10:40:35 +0900

    fix bug that can occur when a reservation endTime = maxEndTime (eg 8pm).  the bug causes the scheduler to accept new reservations for pool/classroom with endTime=maxEndTime even when no space is available

--- src/utils/reservation-queries.ts
+++ src/utils/reservation-queries.ts
@@ -1,7 +1,7 @@
 import type { Submission } from '$types';
 import type { SettingsManager } from '$lib/client/settings';
 import { OWTime, ReservationCategory } from '$types';
-import { startTimes, endTimes } from '$lib/reservationTimes';
+import { getStartEndTimes } from '$lib/reservationTimes';
 import { timeStrToMin } from '$lib/datetimeUtils';
 
 // helper for getTimeOverlapFilters: sorts all possible start and end times into four groups:
@@ -15,16 +15,14 @@ function getTimeSlots({
 }: {
 	settings: SettingsManager;
 	date: string;
-	category: ReservationCategory.pool;
+	category: ReservationCategory;
 	start: string;
 	end: string;
 }) {
-	let sTs = startTimes(settings, date, category);
-	let eTs = endTimes(settings, date, category);
-	let times = [...sTs, eTs[eTs.length - 1]];
-
+	let times = getStartEndTimes(settings, date, category);
 	let sIdx = times.indexOf(start);
 	let eIdx = times.indexOf(end);
+
 	if (sIdx == -1 && eIdx == -1) {
 		return null;
 	}
@@ -116,8 +114,9 @@ export function getTimeOverlapFilters(settings: SettingsManager, rsv: Submission
 		throw new Error('invalid reservation category: ' + rsv.category);
 	}
 
-	//TODO: fix type
-	const filters: { [key: string]: any }[] = [];
+	type XataFilter = { [key: string]: string | string[] | XataFilter | XataFilter[] };
+
+	const filters: XataFilter[] = [];
 
 	if (owTimes.length > 0) {
 		filters.push({
@@ -126,30 +125,32 @@ export function getTimeOverlapFilters(settings: SettingsManager, rsv: Submission
 		});
 	}
 
-	let slots = getTimeSlots({
-		settings,
-		date: rsv.date,
-		category: ReservationCategory.pool,
-		start,
-		end
-	});
-	if (slots != null) {
-		let timeFilt: { [key: string]: any }[] = [];
-		if (slots.startVals.length > 0) {
-			timeFilt.push({ startTime: { $any: slots.startVals } });
-		}
-		if (slots.endVals.length > 0) {
-			timeFilt.push({ endTime: { $any: slots.endVals } });
-		}
-		if (slots.beforeStart.length > 0 && slots.afterEnd.length > 0) {
-			timeFilt.push({
-				$all: [{ startTime: { $any: slots.beforeStart } }, { endTime: { $any: slots.afterEnd } }]
+	for (let cat of [ReservationCategory.pool, ReservationCategory.classroom]) {
+		let slots = getTimeSlots({
+			settings,
+			date: rsv.date,
+			category: cat,
+			start,
+			end
+		});
+		if (slots != null) {
+			let timeFilt: XataFilter[] = [];
+			if (slots.startVals.length > 0) {
+				timeFilt.push({ startTime: { $any: slots.startVals } });
+			}
+			if (slots.endVals.length > 0) {
+				timeFilt.push({ endTime: { $any: slots.endVals } });
+			}
+			if (slots.beforeStart.length > 0 && slots.afterEnd.length > 0) {
+				timeFilt.push({
+					$all: [{ startTime: { $any: slots.beforeStart } }, { endTime: { $any: slots.afterEnd } }]
+				});
+			}
+			filters.push({
+				category: cat,
+				$any: timeFilt
 			});
 		}
-		filters.push({
-			category: { $any: [ReservationCategory.pool, ReservationCategory.classroom] },
-			$any: timeFilt
-		});
 	}
 	return filters;
 }
