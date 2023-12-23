611b957fe891bf7f0a76a99fb789dfe7b78e9179
Neil <get.neilmolina@gmail.com>
2023-12-23 20:41:01 +0800

    save

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB6j8kYAR977Iy0HHvf3k6yzu1f0_2Cl7I",
+  authDomain: "freedive-superhome.firebaseapp.com",
+  projectId: "freedive-superhome",
+  storageBucket: "freedive-superhome.appspot.com",
+  messagingSenderId: "1021894750641",
+  appId: "1:1021894750641:web:92c2f45e95e328a619cd90",
+  measurementId: "G-ZSSJMX2ZK7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, app };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -48,13 +48,9 @@
 
 	let intervalId: number | undefined;
 
-	$: if ($loginState === 'out' && $page.route.id != '/') {
-		goto('/');
+	$: if ($loginState === 'out' && $page.route.id != '/login') {
+		goto('/login');
 	}
-	$: isFacebook =
-		typeof window !== 'undefined' && window.navigator
-			? navigator.userAgent.includes('FBAN') || navigator.userAgent.includes('FBAV')
-			: false;
 
 	let isLoading = false;
 
@@ -155,13 +151,11 @@
 	};
 
 	onMount(() => {
-		if (!isFacebook) {
-			// @ts-ignore
-			toast.promise(initApp(), {
-				loading: 'loading...',
-				error: 'Loading error'
-			});
-		}
+		// @ts-ignore
+		toast.promise(initApp(), {
+			loading: 'loading...',
+			error: 'Loading error'
+		});
 
 		return () => {
 			if (intervalId) {
@@ -177,38 +171,11 @@
 <Sidebar />
 <div id="app" class="flex px-1 mx-auto w-full">
 	<main class="lg:ml-72 w-full mx-auto">
-		{#if $loginState === 'in'}
-			<slot />
-		{:else}
-			{#if $loginState === 'pending'}
-				<div class="m-auto flex items-center justify-center pt-10">
-					<Spinner />
-				</div>
-			{/if}
-			<div class="{loginVisible($loginState)} m-auto flex items-center justify-center pt-10">
-				<FacebookAuth
-					appId={PUBLIC_FACEBOOK_APP_ID}
-					on:auth-success={getUserFromAuth}
-					on:auth-info={getUserFromAuth}
-				/>
-			</div>
-		{/if}
+		<slot />
 	</main>
 </div>
 
 <Toaster />
-
-{#if isFacebook}
-	<article class="fixed text-center top-0 w-full h-full bg-orange-400 p-20">
-		<h1 class="font-bold">Please don't use Facebook browser</h1>
-		<br />
-		<p>To use default browser:</p>
-		<ul>
-			<li><b>Android -</b> tap in the upper right-hand corner</li>
-			<li><b>iOS -</b> tap in the lower right-hand corner</li>
-		</ul>
-	</article>
-{/if}
 
 <Popup />
 
