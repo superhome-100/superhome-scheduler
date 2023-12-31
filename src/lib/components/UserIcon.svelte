3b913f7b73c0df918043e2fb1c56a0c2ed4bf7ca
Neil <get.neilmolina@gmail.com>
2023-12-31 16:30:46 +0800

    fix

--- src/lib/components/UserIcon.svelte
+++ src/lib/components/UserIcon.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div>
-	{#if $sessionAuth.photoURL}
-		<img class="rounded-[50%] w-10" alt="profilePicture" src={$sessionAuth.photoURL} />
+	{#if $sessionAuth?.photoURL}
+		<img class="rounded-[50%] w-10" alt="profilePicture" src={$sessionAuth?.photoURL} />
 	{:else}
-		<div class="text-xs">{$sessionAuth.displayName || 'user'}</div>
+		<div class="text-xs">{$sessionAuth?.displayName || 'loading...'}</div>
 	{/if}
 </div>
