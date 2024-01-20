9d214294247182d6a43400817b3217b71c5119b5
Neil <get.neilmolina@gmail.com>
2024-01-20 11:02:50 +0800

    try fix

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { auth } from '../../lib/firebase';
 	import { FacebookAuthProvider, signInWithRedirect, signInWithPopup } from 'firebase/auth';
+
+	let session: 'loading' | 'in' | 'out' = 'loading';
 
 	// const loginWithGoogle = async () => {
 	//   const googleProvider = new GoogleAuthProvider();
@@ -15,16 +18,29 @@
 				await signInWithPopup(auth, facebookProvider);
 			} catch (error) {
 				console.error(error);
+				await signInWithRedirect(auth, facebookProvider);
 			}
 		} else {
 			// Use redirect for other browsers
 			await signInWithRedirect(auth, facebookProvider);
 		}
 	};
+
+	onMount(() => {
+		return auth.onIdTokenChanged(async (user) => {
+			if (!user) {
+				session = 'out';
+			} else {
+				session = 'in';
+			}
+		});
+	});
+
+
 </script>
 
 <div class="flex flex-col text-black w-48 gap-2">
 	<h3 class="text-white">Login with</h3>
 	<!-- <button on:click={loginWithGoogle}>Google</button> -->
-	<button on:click={loginWithFacebook}>Facebook</button>
+	<button disabled={['loading','in'].includes(session)} on:click={loginWithFacebook}>Facebook</button>
 </div>
