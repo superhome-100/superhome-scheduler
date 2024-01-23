d038a8bb3c14189657ef64619d263490e480d1ec
Neil <get.neilmolina@gmail.com>
2024-01-23 20:02:08 +0800

    move to node-fetch

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,11 +1,12 @@
-import axios from 'axios';
+import fetch from 'node-fetch';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-	const { pathname, search } = new URL(event.url);
-	if (pathname.startsWith('/__/')) {
+  const { pathname, search } = new URL(event.url);
+  if (pathname.startsWith('/__/')) {
     const redirectUrl = new URL(`https://freedive-superhome.firebaseapp.com${pathname}`);
     redirectUrl.search = search; // copy URL parameters
+
     // Filter out problematic headers
     const headers = Object.fromEntries(
       Object.entries(event.request.headers).filter(([key]) => 
@@ -13,30 +14,29 @@ export async function handle({ event, resolve }) {
       )
     );
 
-		const isJsFile = pathname.endsWith('.js');
-    const axiosOptions = isJsFile ? { headers, responseType: 'arraybuffer' } : { headers };
-
-    const proxyResponse = await axios.get(redirectUrl.toString(), {
-			...axiosOptions,
-		});
-		let responseData = await proxyResponse.data
-		if (isJsFile) {
-			// If it's a JS file, read the data as an ArrayBuffer and convert it to a Blob
-			const arrayBuffer = await proxyResponse.data;
-			responseData = new Blob([arrayBuffer], { type: proxyResponse.headers.get('content-type') });
-		}
+    const proxyResponse = await fetch(redirectUrl.toString(), { headers });
     const proxyHeaders = Object.fromEntries(
       Object.entries(proxyResponse.headers).map(([key, value]) => [key, String(value)])
     );
     const { status } = proxyResponse;
 
+    let responseData;
+    if (pathname.endsWith('.js')) {
+      // If it's a JS file, read the data as an ArrayBuffer and convert it to a Blob
+      const arrayBuffer = await proxyResponse.arrayBuffer();
+      responseData = new Blob([arrayBuffer], { type: proxyHeaders['content-type'] });
+    } else {
+      // Otherwise, just use the text data
+      responseData = await proxyResponse.text();
+    }
+
     return new Response(responseData, {
       status,
       headers: proxyHeaders
     });
-	}
+  }
 
-	const response = await resolve(event)
+  const response = await resolve(event);
 
-	return response;
+  return response;
 }
