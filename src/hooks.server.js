3562b4fbf4abb15d19ab8eae4565a37d0655c794
Neil <get.neilmolina@gmail.com>
2024-01-23 18:22:09 +0800

    test

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
 	const { pathname, search } = new URL(event.url);
@@ -5,11 +7,11 @@ export async function handle({ event, resolve }) {
     const redirectUrl = new URL(`https://freedive-superhome.firebaseapp.com${pathname}`);
     redirectUrl.search = search; // copy URL parameters
 
-    const proxyResponse = await fetch(redirectUrl.toString(), event.request);
-    const proxyHeaders = new Headers(proxyResponse.headers);
+    const proxyResponse = await axios.get(redirectUrl.toString(), { headers: event.request.headers });
+    const proxyHeaders = proxyResponse.headers;
     const { status } = proxyResponse;
 
-    return new Response(proxyResponse.body, {
+    return new Response(proxyResponse.data, {
       status,
       headers: proxyHeaders
     });
