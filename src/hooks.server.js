eb8dd1190307366e49e3b81ddc58be7a1c0b49d8
Neil <get.neilmolina@gmail.com>
2024-01-23 18:17:36 +0800

    fix

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,15 +2,17 @@
 export async function handle({ event, resolve }) {
 	const { pathname, search } = new URL(event.url);
 	if (pathname.startsWith('/__/')) {
-		const redirectUrl = new URL(`https://freedive-superhome.firebaseapp.com${pathname}`);
-		redirectUrl.search = search; // copy URL parameters
-		return new Response(null, {
-			status: 302,
-			headers: {
-				...event.request.headers,
-				location: redirectUrl.toString()
-			}
-		})
+    const redirectUrl = new URL(`https://freedive-superhome.firebaseapp.com${pathname}`);
+    redirectUrl.search = search; // copy URL parameters
+
+    const proxyResponse = await fetch(redirectUrl.toString(), event.request);
+    const proxyHeaders = new Headers(proxyResponse.headers);
+    const { status } = proxyResponse;
+
+    return new Response(proxyResponse.body, {
+      status,
+      headers: proxyHeaders
+    });
 	}
 
 	const response = await resolve(event)
