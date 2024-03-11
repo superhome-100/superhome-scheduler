d41d00e74969952a7d01cb1dcb29c7295f4caa62
Neil <get.neilmolina@gmail.com>
2024-03-11 08:29:03 +0800

    remove o2 and short session

--- src/lib/components/ResFormOpenWater.svelte
+++ src/lib/components/ResFormOpenWater.svelte
@@ -213,33 +213,5 @@
 				<label for="formLargeBuoy">large buoy</label>
 			</div>
 		{/if}
-		<div>
-			{#if disabled}
-				<input type="hidden" name="O2OnBuoy" value={o2OnBuoy ? 'on' : 'off'} />
-			{/if}
-			<input
-				type="checkbox"
-				id="formO2OnBuoy"
-				name="O2OnBuoy"
-				checked={o2OnBuoy}
-				{disabled}
-				tabindex="5"
-			/>
-			<label for="formO2OnBuoy">O2 on buoy (additional fee)</label>
-		</div>
-		<div>
-			{#if disabled}
-				<input type="hidden" name="shortSession" value={shortSession ? 'on' : 'off'} />
-			{/if}
-			<input
-				type="checkbox"
-				id="formShortSession"
-				name="shortSession"
-				checked={shortSession}
-				{disabled}
-				tabindex="5"
-			/>
-			<label for="formShortSession">short session</label>
-		</div>
 	</div>
 </ResFormGeneric>
