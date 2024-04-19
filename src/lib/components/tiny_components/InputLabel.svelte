d813ba1c6fce4b17fc6ae5acf32957529f289598
Neil <get.neilmolina@gmail.com>
2024-04-19 11:15:57 +0800

    new form submission layout (#83)
    * new form layout
    
    * partial reservation

---
+++ src/lib/components/tiny_components/InputLabel.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let label = '';
+	export let forInput = '';
+</script>
+
+<li class="flex mt-2">
+	<div class="w-[100px] dark:text-white text-right mt-2"><label for={forInput}>{label}</label></div>
+	<div class="flex-1">
+		<slot />
+	</div>
+</li>
