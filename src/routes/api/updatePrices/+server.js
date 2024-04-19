048597694083b394c095017039cdc124a3b0867a
Neil <get.neilmolina@gmail.com>
2024-04-19 15:25:04 +0800

    add pricing

--- src/routes/api/updatePrices/+server.js
+++ src/routes/api/updatePrices/+server.js
@@ -23,7 +23,9 @@ const unpackTemplate = (uT) => {
 			course: uT.priceTemplate.coachOW,
 			autonomous: uT.priceTemplate.autoOW,
 			cbs: uT.priceTemplate.cbsOW,
-			proSafety: uT.priceTemplate.proSafetyOW
+			proSafety: uT.priceTemplate.proSafetyOW,
+			autonomousPlatform: uT.priceTemplate.platformOW,
+			autonomousPlatformCBS: uT.priceTemplate.platformCBSOW
 		}
 	};
 };
