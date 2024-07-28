4a0695c04c99a639439f49acbc24c50d7fe8e78b
Neil <get.neilmolina@gmail.com>
2024-07-28 21:20:52 +0800

    fir url path generation for single-day

--- src/lib/components/DayOpenWaterV2.svelte
+++ src/lib/components/DayOpenWaterV2.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-	import { adminComments, viewMode, owUpdateStates, buoys, syncBuoys, reservations as reservationsStore } from '$lib/stores';
+	import {
+		adminComments,
+		viewMode,
+		owUpdateStates,
+		buoys,
+		syncBuoys,
+		reservations as reservationsStore
+	} from '$lib/stores';
 	import { getContext, onMount } from 'svelte';
 	import AdminComment from '$lib/components/AdminComment.svelte';
 	import RsvTabs from '$lib/components/RsvTabs.svelte';
