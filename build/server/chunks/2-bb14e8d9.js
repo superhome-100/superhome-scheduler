e2e9fd9bb7f4d9f767bf784cae3031019aeadc2e
neil paul molina <npm@neils-MacBook-Air.local>
2025-09-13 11:44:35 +0800

    remove folders

--- build/server/chunks/2-bb14e8d9.js
+++
@@ -1,9 +0,0 @@
-const index = 2;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_layout.svelte-a1068c59.js')).default;
-const imports = ["_app/immutable/nodes/2.576bde49.js","_app/immutable/chunks/index.8e4fed50.js","_app/immutable/chunks/firebase.81e41ff6.js","_app/immutable/chunks/singletons.daa49798.js"];
-const stylesheets = ["_app/immutable/assets/app.3f7ca999.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=2-bb14e8d9.js.map
