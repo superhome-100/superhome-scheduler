5fc80af89dcd28bb34cfcc5184a5a9db8abea286
Mate Pek <mate.pek@gmail.com>
2026-02-04 12:29:53 +0800

    small fix

--- src/utils/reservations.ts
+++ src/utils/reservations.ts
@@ -3,9 +3,9 @@ import type { Submission } from '$types';
 export const getNumberOfOccupants = (rsvs: Submission[]) =>
 	rsvs.reduce((n, rsv) => {
 		if (rsv.category === 'classroom') {
-			return n + rsv.numStudents;
+			return n + (rsv.numStudents ?? 0);
 		} else {
-			return rsv.resType === 'course' ? n + 2 * Math.ceil(rsv.numStudents / 2) : n + 1;
+			return rsv.resType === 'course' ? n + 2 * Math.ceil(rsv.numStudents ?? 0 / 2) : n + 1;
 		}
 	}, 0);
 
