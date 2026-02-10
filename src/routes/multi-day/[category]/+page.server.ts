97835137a8e801d2f22e47c2c8ef8325f6ecc353
Mate Pek <mate.pek@gmail.com>
2026-02-10 12:23:42 +0800

    migration to SvelteKit 2

--- src/routes/multi-day/[category]/+page.server.ts
+++ src/routes/multi-day/[category]/+page.server.ts
@@ -2,5 +2,5 @@ import { getYYYYMM } from "$lib/datetimeUtils.js";
 import { redirect } from "@sveltejs/kit";
 
 export function load({ url }) {
-    throw redirect(307, url.pathname + "/" + getYYYYMM(new Date()));
+    redirect(307, url.pathname + "/" + getYYYYMM(new Date()));
 }
